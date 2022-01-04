684a870794e601f1fe825d21081fb5cae63199c3
Chris Insignares <insignaresc115@gmail.com>
2022-01-04 07:09:52 -0500

    type defs updated

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,9 +3,9 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
     type Book {
         _id: ID:
-        authors: String
+        authors: [String]
         description: String!
-        bookID: String!
+        bookID: ID!
         image: String
         link: String
         title: String!
@@ -22,8 +22,25 @@ const typeDefs = gql`
         user: User
     }
     type Query {
-        users: [User]
-        user(username: String!): User
-        saveBook: Book
+        me: User
     }
+    input BookInput {
+        authors: [String]
+        description: String!
+        bookId: ID!
+        image: String
+        link: String
+        title: String!
+    }
+    type Mutation {
+    # saveBook: Book
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    # next queries will use context becuase we are already logged in
+    saveBook(bookData: BookInput!): User
+    deleteBook(bookId: ID!): User
+  }
+    
     `;
+
+module.exports = typeDefs;
