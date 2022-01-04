241ba8b07847d63aea0cd831c842e11572bad354
Chris Insignares <insignaresc115@gmail.com>
2022-01-04 08:01:37 -0500

    login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,14 +1,36 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { User, Book } = require("../models");
-// const { signToken } = require("../utils/auth");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find();
+    me: async (parents, args, context) => {
+      // if they is a tekn
+      if (context.user) {
+        return await User.findOne({ _id: context.user._id }).select(
+          "-password"
+        );
+      }
+      throw new AuthenticationError("You're logged in");
     },
-    saveBook: async () => {
-      return Book.find();
+  },
+  Mutation: {
+    addUser: async (parent, args, context) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { user, token };
+    },
+    login: async (parent, { email, password }, context) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw AuthenticationError("Cannot find user");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw AuthenticationError("Is not correct password");
+      }
+      const token = signToken(user);
+      return { user, token };
     },
   },
 };
